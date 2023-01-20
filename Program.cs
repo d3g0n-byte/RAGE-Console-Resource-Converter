915c78c0275699bfb856df865a8736dada146138
d3g0n-byte <108334730+d3g0n-byte@users.noreply.github.com>
2023-01-21 01:27:06 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,96 +8,136 @@ using Converter.Utils;
 using System.Numerics;
 using System.Collections;
 using Converter;
-using Converter.openformat;
+using Converter.openFormats;
+using static Converter.ResourceUtils;
+using ICSharpCode.SharpZipLib.Zip.Compression;
+using System.Security.Cryptography;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 
 namespace ConsoleApp1
 {
 	internal class Program
 	{
-		public static int cpuSize;
-		public static string fileName;
-		public static string fileMask;
 		static unsafe void Main(string[] args)
 		{
 			//Array.Resize<string>(ref args, 1);
-			Program.fileName = args[0];
-			//{
-				//IV_Animation.ExportWAD();
-				//return;
-			//}
-			//Program.fileName = "C:\\Users\\d3g0n\\Desktop\\3d\\tile_146_0.xvd";
-			if (args.Length < 1) return;
-			fileMask = Other.GetFileMask(fileName);
-			FileStream file;
-			file = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.None);
-			EndianBinaryReader br = new EndianBinaryReader(file);
-			bool isCompressed = true;
-			br.Endianness = Endian.BigEndian;
-			if (isCompressed)
+			//args[0] = "C:\\Users\\d3g0n\\Desktop\\3d\\player_elegantsuit.xft";
+			//args[0] = "C:\\Users\\d3g0n\\source\\repos\\Converter\\bin\\x64\\Debug\\net7.0\\test.xvd"; // собраная из нуля модель
+			//args[0] = "C:\\Users\\d3g0n\\Desktop\\3d\\tile_130_10.xvd";
+			//args[0] = "C:\\Users\\d3g0n\\Desktop\\3d\\steamer01x_hilod.xfd";
+			//args[0] = "C:\\Users\\d3g0n\\Desktop\\3d\\pln_derrick02x_oilrig02anim_hilod.xtd";
+			//args[0] = "C:\\Users\\d3g0n\\Desktop\\3d\\swterrain.xsi";
+
+			if (args.Length < 1){ Console.WriteLine("Usage: Converter.exe input_file"); return; }
+			Converter.FileInfo.fileName = args[0];
+			using (EndianBinaryReader br = new EndianBinaryReader(File.OpenRead(Converter.FileInfo.fileName)))
 			{
-				uint magic = br.ReadUInt32();
-				if (magic == 0x52534305 || magic == 0x52534385 || magic == 0x52534306 || magic == 0x52534386 || magic == 0x52534337) br.Endianness = Endian.LittleEndian;
+				RSCHeader hdr = new RSCHeader();
+				byte[] magic = br.ReadBytes(4);
+				if(magic[0] != 82)
+				{
+					Array.Reverse(magic, 0, magic.Length);
+					br.Endianness = Endian.BigEndian;
+				}
+				hdr.Magic = Encoding.ASCII.GetBytes(Encoding.Default.GetString(magic.Take(3).ToArray()));
+				hdr.Version = magic[3];
 
-				uint version = br.ReadUInt32();
-				uint flags = br.ReadUInt32();
-				uint secondFlags = 0;
-				
-				if (magic == 0x85435352 || magic == 0x86435352 || magic == 0x37435352) secondFlags = br.ReadUInt32();
-				cpuSize = 0;
-				int gpuSize = 0;
-				if (magic == 0x05435352 || magic == 0x06435352)// mc:la iv rdr mp3
-				{
-					cpuSize = XCompress.GetCPUSizeRSC5(flags);
-					gpuSize = XCompress.GetGPUSizeRSC5(flags);
-				}
-				else if (magic == 0x85435352 || magic == 0x86435352)// rdr
-				{
-					cpuSize = XCompress.GetCPUSizeRSC85(flags, secondFlags);
-					gpuSize = XCompress.GetGPUSizeRSC85(flags, secondFlags);
-				}
-				else if (magic == 0x37435352)// v
-				{
-					cpuSize = XCompress.GetSizeRSC37(flags, 0x2000);
-					gpuSize = XCompress.GetSizeRSC37(secondFlags, 0x2000);
-				}
-				else return;
-
+				if (Encoding.Default.GetString(hdr.Magic) != "RSC") return;
+				ResourceUtils.ResourceInfo.version = br.ReadInt32();
+				ResourceUtils.FlagInfo.Flag1 = br.ReadInt32();
+				if (hdr.Version == 0x85 || hdr.Version == 0x86 || hdr.Version == 0x37)
+					ResourceUtils.FlagInfo.Flag2 = br.ReadInt32();
 				uint cmagic = br.ReadUInt32();
-				if ((cmagic & 0xFFFF0000) != 0x0FF50000) { Console.WriteLine("It's not lzx."); return; }
-				int csize = br.ReadInt32();
-				int usize = csize * 4;
-				byte[] origBuffer = br.ReadBytes(csize);
-				file.Close();
-				// min. size
-				if (usize < 203968) usize = 203968;
-				byte[] decompBuffer = XCompress.Decompress(origBuffer, csize, usize, false);
-				Array.Resize<byte>(ref decompBuffer, cpuSize + gpuSize);
-				//string decompressedFileName = fileName + ".tmp";
-				//File.WriteAllBytes(decompressedFileName, decompBuffer);
-				MemoryStream memStream = new MemoryStream(decompBuffer);
-				if (magic == 0x85435352 && version == 133)
+				if ((cmagic & 0xFFFF0000) != 0x0FF50000)
 				{
-					if (!RDR_VolumeData.ReadVolumeData(memStream, true))
+					// it's not a XCompress so move the position back
+					br.BaseStream.Position -= 4;
+
+					int dLen = FlagInfo.BaseResourceSizeP + FlagInfo.BaseResourceSizeV;
+					byte[] origBuffer = br.ReadBytes(Convert.ToInt32(br.BaseStream.Length - br.BaseStream.Position));
+					byte[] decompBuffer = DataUtils.DecompressDeflate(origBuffer, dLen, noHeader: false);
+					ReadMemoryData(hdr, decompBuffer);
+					byte[] sys = new byte[FlagInfo.BaseResourceSizeV];
+					byte[] gfx = new byte[FlagInfo.BaseResourceSizeP];
+					Buffer.BlockCopy(decompBuffer, 0, sys, 0, sys.Length);
+					Buffer.BlockCopy(decompBuffer, sys.Length, gfx, 0, gfx.Length);
+					File.WriteAllBytes($"{Converter.FileInfo.filePath}\\{Converter.FileInfo.baseFileName}.sys", sys);
+					File.WriteAllBytes($"{Converter.FileInfo.filePath}\\{Converter.FileInfo.baseFileName}.gfx", gfx);
+				}
+				else
+				{
+					int csize = br.ReadInt32();
+					int usize = csize * 4;
+					if (usize < 203968) usize = 203968;
+					byte[] origBuffer = br.ReadBytes(csize);
+					byte[] decompBuffer = XCompress.Decompress(origBuffer, csize, usize, false);
+					// 
+					//byte[] sys = new byte[FlagInfo.BaseResourceSizeV];
+					//byte[] gfx = new byte[FlagInfo.BaseResourceSizeP];
+					//Buffer.BlockCopy(decompBuffer, 0, sys, 0, sys.Length);
+					//Buffer.BlockCopy(decompBuffer, sys.Length, gfx, 0, gfx.Length);
+					//File.WriteAllBytes($"{Converter.FileInfo.filePath}\\{Converter.FileInfo.baseFileName}.sys", sys);
+					//File.WriteAllBytes($"{Converter.FileInfo.filePath}\\{Converter.FileInfo.baseFileName}.gfx", gfx);
+					//
+					ReadMemoryData(hdr, decompBuffer);
+				}
+			}
+		}
+		private static void ReadMemoryData(RSCHeader hdr, byte[] data)
+		{
+			using (MemoryStream ms = new MemoryStream(data))
+			{
+				if (hdr.Version == 0x85 && ResourceUtils.ResourceInfo.version == 133)
+				{
+					if (!RDR_VolumeData.ReadVolumeData(ms, true))
 					{
-						Console.WriteLine("Error");
-						Console.ReadKey();
+						throw new Exception("Error while reading RDR model data.");
 					}
 				}
-				/*else if (magic == 0x05435352 && version == 6)
+				/*
+				else if (rsc.m_Header.Version == 5 && rsc.m_ResourceVersion == 6)
 				{
-					if (!RDR_Animation.Animation(memStream, true))
+					if (!RDR_Animation.Animation(ms, true))
 					{
-						Console.WriteLine("Error to export anims");
-						Console.ReadKey();
+						throw new Exception("Error while reading RDR animation data");
 					}
-
-				}*/
-
-
-
-				return;
-
+				}
+				*/
+				else if (hdr.Version == 0x85 && ResourceUtils.ResourceInfo.version == 138)
+				{
+					if (!RDR_Fragment.ReadFragment(ms, true))
+					{
+						throw new Exception("Error while reading RDR fragment model.");
+					}
+				}
+				else if (hdr.Version == 0x85 && ResourceUtils.ResourceInfo.version == 1)
+				{
+					if (!RDR_FragmentDictionary.ReadFragmentDictionary(ms, true))
+					{
+						throw new Exception("Error while reading RDR Fragment Dictionary.");
+					}
+				}
+				else if (hdr.Version == 0x85 && ResourceUtils.ResourceInfo.version == 10)
+				{
+					EndianBinaryReader br = new EndianBinaryReader(ms);
+					if (true) br.Endianness = Endian.BigEndian;// 0 - lit, 1 - big
+					if (!TextureDictionary.ReadTextureDictionary(br, (uint)ResourceUtils.FlagInfo.RSC85_ObjectStart))
+					{
+						throw new Exception("Error while reading RDR Texture Dictionary.");
+					}
+					br.Close();
+				}
 			}
+		}
+
+	}
+	public class RSCHeader
+	{
+		public byte[] Magic = new byte[3];
+		public byte Version;
+		public RSCHeader()
+		{
 		}
 	}
 }
